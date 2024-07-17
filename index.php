b06e353201851035d19b84966e4f4c20956e02dc
shohjahon <shohjahon>
2024-07-17 18:01:30 +0500

    Hello World

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO App</title>
+</head>
+<body>
+    <form action="main.php">
+        <input type="checkbox" type="text" name="checkbox1" id="checkbox1"> 
+        <input type="text" name="checbox1.1" id="checbox1.1">
+    </form>    
+</body>
+</html>
