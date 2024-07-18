f81a9d0b99bf955eb36f3e96770f9aa6707ab3c4
shohjahon <shohjahon>
2024-07-19 04:44:33 +0500

    feat: MyTodoProject

--- View.php
+++ View.php
@@ -41,6 +41,21 @@ if (isset($_GET['id'])) {
 
 $usersInfo = $db->SendAllUsers();
 
+$itemsPerPage = 5;
+$totalItems = count($usersInfo);
+$totalPages = ceil($totalItems / $itemsPerPage);
+
+$currentPage = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+if ($currentPage < 1) {
+    $currentPage = 1;
+} elseif ($currentPage > $totalPages) {
+    $currentPage = $totalPages;
+}
+
+$offset = ($currentPage - 1) * $itemsPerPage;
+
+$currentItems = array_slice($usersInfo, $offset, $itemsPerPage);
+
 ?>
 
 <!DOCTYPE html>
@@ -89,7 +104,7 @@ $usersInfo = $db->SendAllUsers();
                             </tr>
                         </thead>
                         <tbody>
-                            <?php foreach ($usersInfo as $userInfo): ?>
+                            <?php foreach ($currentItems as $userInfo): ?>
                                 <tr>
                                     <td class="<?php echo $userInfo['status'] ? 'completed' : ''; ?>">
                                         <?php echo htmlspecialchars($userInfo['todos']); ?>
@@ -109,10 +124,30 @@ $usersInfo = $db->SendAllUsers();
                             <?php endforeach; ?>
                         </tbody>
                     </table>
-                    <div>
+                    <div class="d-flex justify-content-between align-items-center">
                         <form action="View.php" method="post">
                             <button type="submit" class="btn btn-primary" name="truncateButton">Truncate</button>
                         </form>
+
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination mb-0">
+                                <li class="page-item <?php if($currentPage <= 1) echo 'disabled'; ?>">
+                                    <a class="page-link" href="<?php if($currentPage > 1) echo '?page='.($currentPage - 1); else echo '#'; ?>" aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <?php for ($i = 1; $i <= $totalPages; $i++): ?>
+                                    <li class="page-item <?php if($i == $currentPage) echo 'active'; ?>">
+                                        <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                    </li>
+                                <?php endfor; ?>
+                                <li class="page-item <?php if($currentPage >= $totalPages) echo 'disabled'; ?>">
+                                    <a class="page-link" href="<?php if($currentPage < $totalPages) echo '?page='.($currentPage + 1); else echo '#'; ?>" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
                     </div>
                 </div>
             </div>
